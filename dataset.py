5aef65fd563e69c05737f547d703d591140ea1d3
Satyam Gupta <guptasatyam1709@gmail.com>
2025-06-19 19:30:10 +0530

    dataset

--- dataset.py
+++ dataset.py
@@ -4,14 +4,16 @@ import cv2
 train_dir = "mirflickr"
 hr_dir = "HR"
 lr_dir = "LR"
- 
+count = 0
 os.makedirs(hr_dir, exist_ok=True)
 os.makedirs(lr_dir, exist_ok=True)
  
 for img in os.listdir(train_dir): 
     img_path = os.path.join(train_dir, img)
     img_arr = cv2.imread(img_path)
-
+    if count > 15000:
+        break
+    count+=1
     # Skip if the image can't be read
     if img_arr is None:
         continue
